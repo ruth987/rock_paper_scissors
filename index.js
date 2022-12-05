15dd90f829533937e5da53a67eca167d5b55e630
Ruth Wossen <123456789+odin@users.noreply.github.com>
2022-12-05 14:43:28 +0300

    complete playOneRound function and organize the accepted user data

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//get the players selection.
+let playerSelection = (prompt("Make a choice: ")).toLowerCase();
+
 //this will return a random item from the arr that contains rock, paper, and scissor.
 function getComputerChoice(){
     const choices = ["rock", "paper", "scissor"];
@@ -7,3 +10,39 @@ function getComputerChoice(){
     const item = choices[randomIndex];
     return item;
 } 
+
+//get the computer selection
+let computerSelection = getComputerChoice();
+
+//a function that plays one round.
+function playOneRound(playerSelection, computerSelection){
+    console.log("Your choice: "+ playerSelection);
+    console.log("Computer choice: "+ computerSelection);
+    
+    if(computerSelection==playerSelection){
+        return "PLAY AGAIN";
+    } 
+    else if(computerSelection=="rock" && playerSelection=="scissor"){
+        return "You lose! Rock beats scissor.";
+    }
+    else if(computerSelection=="scissor" && playerSelection=="rock"){
+        return "You won! Rock beats scissor.";
+    }
+    else if(computerSelection=="scissor" && playerSelection=="paper"){
+        return "You lose! scissor beats paper.";
+    }
+    else if(computerSelection=="paper" && playerSelection=="scissor"){
+        return "You won! scissor beats paper";
+    }
+    else if(computerSelection=="rock" && playerSelection=="paper"){
+        return "You won! paper beats rock.";
+    }
+    else if(computerSelection=="paper" && playerSelection=="rock"){
+        return "You lose! paper beats rock.";
+    }
+    else {
+        return "You can only insert 'rock', 'paper' or 'scissor'!!";
+    }
+    
+}
+
